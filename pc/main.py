8cf06c5005740acc3463d81141969640c410f873
klangner <klangner@gmail.com>
2015-08-30 16:13:28 +0200

    Reset board before connectingto usb port

--- pc/main.py
+++ pc/main.py
@@ -3,11 +3,12 @@ import time
 
 
 def main():
-    romeo = serial.Serial('/dev/ttyACM0', 57600, timeout=1)
+    reset()
+    hcr = openConnection()
     count = 0
     step = 1;
     while True:
-        line = romeo.readline()
+        line = hcr.readline()
         if len(line) > 0:
             print(str(time.time()) + ": " + line)
             count += step
@@ -15,7 +16,17 @@ def main():
                 step = -step
             motors = "%d %d\n" % (count, count)
             print("Python: " + motors)
-            romeo.write(motors)
+            hcr.write(motors)
 
+def openConnection():
+    return serial.Serial('/dev/ttyACM0', 57600, timeout=1)
 
+def reset():
+    ser = serial.Serial('/dev/ttyACM0', 1200)
+    ser.setRTS(True)  # RTS line needs to be held high and DTR low
+    ser.setDTR(False) # (see Arduino IDE source code)
+    ser.close()
+    time.sleep(1)
+
+# Start script
 main()
--- sandbox/src/main.cpp
+++ sandbox/src/main.cpp
@@ -9,30 +9,6 @@ void setup() {
   Serial.println("Ready.");
 }
 
-void loop2(){
-  if(Serial.available() == 5){
-    if(Serial.read() == HEADER){
-      char leftDir = Serial.read();
-      int leftSpeed = int(Serial.read());
-      if(leftDir == '-')
-        leftSpeed = -leftSpeed;
-      char rightDir = Serial.read();
-      int rightSpeed = int(Serial.read());
-      if(rightDir == '-')
-        rightSpeed = -rightSpeed;
-
-      Serial.print("Arduino: ");
-      Serial.print(leftSpeed);
-      Serial.print(" ");
-      Serial.print(rightSpeed);
-      Serial.println();
-    }
-  }
-
-  delay(DELTA_TIME);
-}
-
-
 void loop(){
   String text = Serial.readStringUntil('\n');
   if(text.length() > 0){
